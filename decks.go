cf8335337154566d1f897929966576c8c3f59209
2020HT66556 <2020HT66556@wilp.bits-pilani.ac.in>
2021-10-10 20:05:33 +0530

    New Feature 2 Added

--- decks.go
+++ decks.go
@@ -69,3 +69,18 @@ func (d deck) shuffle() {
 	}
 
 }
+
+
+
+################
+##Feature 2
+################
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
